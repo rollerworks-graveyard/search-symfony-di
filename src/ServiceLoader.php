aad0d735aabcd115bd3a4ff1f5e3a86b370e90e2
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-07-08 14:13:35 +0200

    make usage of this extension easier
    * Provide service definitions for registering
    * Provide ServiceLoader for loading the service-files

---
+++ src/ServiceLoader.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the RollerworksSearch Component package.
+ *
+ * (c) 2014 Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Rollerworks\Component\Search\Extension\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * ServiceLoader Helps with the registering of services at
+ * the ContainerBuilder.
+ *
+ * @author Sebastiaan Stok <s.stok@rollerscapes.net>
+ */
+class ServiceLoader
+{
+    /**
+     * @var XmlFileLoader
+     */
+    private $loader;
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function __construct(ContainerBuilder $container)
+    {
+        $this->loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    }
+
+    /**
+     * @param $serviceFile
+     *
+     * @return self
+     */
+    public function loadFile($serviceFile)
+    {
+        $this->loader->load($serviceFile.'.xml');
+
+        return $this;
+    }
+}
